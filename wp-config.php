be049b5f739b4cf32f182e62a95a381ae0f86134
root <root@gongpingjia0.(none)>
2015-08-15 14:55:40 +0800

    [REV]Change db host to aliyun db

--- wp-config.php
+++ wp-config.php
@@ -21,7 +21,7 @@ define('DB_USER', 'pingjia');
 /** MySQL 数据库密码 */
 define('DB_PASSWORD', 'De32wsxC');
 /** MySQL 主机 */
-define('DB_HOST', '211.149.206.212');
+define('DB_HOST', '10.51.178.7');
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8');
 /** 数据库整理类型。如不确定请勿更改 */
